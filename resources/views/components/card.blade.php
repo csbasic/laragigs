c52b202cda8c9622b9f23adf6cf72f0aa55f8c04
Chi Serge <chijokae@gmail.com>
2024-03-29 10:44:55 +0100

    Update done on card component | space on posts on small devices

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->merge(['class' => 'bg-gray-50 p-6 border border-gray-200 !mt-0 rounded']) }}>
+<div {{ $attributes->merge(['class' => 'bg-gray-50 p-6 border border-gray-200 lg:!mt-0  rounded']) }}>
    {{ $slot }}
 </div>
